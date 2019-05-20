df15c5f835aca3ecaa41ff86d7faa3ad1cbbcb7a
佐藤大地 <sato.daichi.2041@members.co.jp>
2019-05-20 12:01:57 +0900

    FavoriteListを作成（途中）

---
+++ app/src/main/java/sato/daichi/techacademy/jp/FavoriteListActivity.kt
@@ -0,0 +1,12 @@
+package sato.daichi.techacademy.jp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_list)
+    }
+}
